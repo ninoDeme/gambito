3b84d6612d6cf5c232b2549ace4e2570dcd221f2
ricardo.demeterko <ninodemeterko@hotmail.com>
2025-05-15 18:15:50 -0300

    wip

--- web/app/pages/dia/[dia].vue
+++ web/app/pages/dia/[dia].vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import { UBadge } from "#components";
+import { NuxtLinkLocale, UBadge, UButton } from "#components";
 import { parseDate } from "@internationalized/date";
 import type { TableColumn } from "@nuxt/ui";
+import { Fragment } from "vue/jsx-runtime";
 const route = useRoute("dia-dia___pt_br");
 const diaAtual = parseDate(route.params.dia);
 const { data, status, error, refresh, clear } = await useFetch(
   `/api/dia/${diaAtual}`,
 );
+const localePath = useLocalePath();
 
 const columns: TableColumn<{
   id: number;
@@ -14,7 +16,22 @@ const columns: TableColumn<{
   produto: number;
 }>[] = [
   {
+    id: "actions",
+    header: () => h("span", { class: "w-max" }, "Ações"),
+    enableHiding: false,
+    enableSorting: false,
+    cell: ({ row }) =>
+      h(UButton, {
+        to: localePath({
+          name: "linha-producao",
+          params: { codigo: row.getValue("id") },
+        }),
+        label: "Editar",
+      }),
+  },
+  {
     accessorKey: "id",
+    id: "id",
     header: "#",
     cell: ({ row }) => `#${row.getValue("id")}`,
   },
@@ -33,13 +50,66 @@ const columns: TableColumn<{
     cell: ({ row }) => row.getValue("produto"),
   },
 ];
+
+const table = useTemplateRef("table");
 </script>
 <template>
   <div>{{ diaAtual?.toString() }}</div>
-  <UButton @click="refresh()">Recarregar</UButton>
-  <UTable
-    :data="data"
-    :loading="status === 'pending'"
-    :columns="columns"
-  ></UTable>
+  <div class="flex-1 divide-y divide-accented w-full">
+    <div class="flex items-center gap-2 px-4 py-3.5 overflow-x-auto">
+      <UInput
+        :model-value="
+          table?.tableApi?.getColumn('id')?.getFilterValue() as string
+        "
+        class="max-w-sm min-w-[12ch]"
+        placeholder="Filtrar..."
+        @update:model-value="
+          table?.tableApi?.getColumn('id')?.setFilterValue($event)
+        "
+      />
+
+      <UButton color="neutral" label="Buscar" @click="refresh()" />
+
+      <UDropdownMenu
+        :items="
+          table?.tableApi
+            ?.getAllColumns()
+            .filter((column) => column.getCanHide())
+            .map((column) => ({
+              label: column.id,
+              type: 'checkbox' as const,
+              checked: column.getIsVisible(),
+              onUpdateChecked(checked: boolean) {
+                table?.tableApi
+                  ?.getColumn(column.id)
+                  ?.toggleVisibility(!!checked);
+              },
+              onSelect(e?: Event) {
+                e?.preventDefault();
+              },
+            }))
+        "
+        :content="{ align: 'end' }"
+      >
+        <UButton
+          label="Colunas"
+          color="neutral"
+          variant="outline"
+          trailing-icon="i-lucide-chevron-down"
+          class="ml-auto"
+          aria-label="Columns select dropdown"
+        />
+      </UDropdownMenu>
+    </div>
+
+    <UTable
+      ref="table"
+      :data="data"
+      :columns="columns"
+      sticky
+      class="h-96"
+      :loading="status === 'pending'"
+    >
+    </UTable>
+  </div>
 </template>
